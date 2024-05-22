24d0395a8f8cef27b050d3c26f337aedae87a173
andres.martinez <andres.martinez@macroproyectos.com>
2024-05-22 17:26:30 -0500

    Inicialización servicio SEED (consulta a API PokeAPI)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 @Module({
@@ -14,7 +15,8 @@ import { CommonModule } from './common/common.module';
       rootPath: join(__dirname, '..', 'public')
     }),
     PokemonModule,
-    CommonModule
+    CommonModule,
+    SeedModule
   ],
 })
 export class AppModule { }
